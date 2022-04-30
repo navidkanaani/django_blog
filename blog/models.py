bed3e7dc1e8ad078a857228f35643f2d2e37ede7
Navid <navidkaen@gmail.com>
2022-04-30 11:46:46 +0430

    Add models, relationships

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=250)
+    img_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.title}, {self.date}, {self.author}, {self.tag}"
--- blog/views.py
+++ blog/views.py
@@ -1,86 +1,27 @@
-from datetime import date
-from unittest import load_tests
-from django.shortcuts import render
-
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Navid",
-        "date": date(2021, 7, 12),
-        "title": "Mountain Hiking",
-        "excerpt":"Theres nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content":"""
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum in fuga quo sunt neque, 
-            minima fugiat provident, doloremque rerum debitis autem saepe aliquid minus sequi, 
-            reiciendis iste vero quod nesciunt?
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum in fuga quo sunt neque, 
-            minima fugiat provident, doloremque rerum debitis autem saepe aliquid minus sequi, 
-            reiciendis iste vero quod nesciunt?
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum in fuga quo sunt neque, 
-            minima fugiat provident, doloremque rerum debitis autem saepe aliquid minus sequi, 
-            reiciendis iste vero quod nesciunt?
-        """
-    },
-    {
-        "slug": "visiting-the-maldives",
-        "image": "maldives.jpg",
-        "author": "Navid",
-        "date": date(2021, 6, 23),
-        "title": "Malidives is perfect!",
-        "excerpt": "If its not the paradise so where is it!?",
-        "content":"""
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum in fuga quo sunt neque, 
-            minima fugiat provident, doloremque rerum debitis autem saepe aliquid minus sequi, 
-            reiciendis iste vero quod nesciunt?
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum in fuga quo sunt neque, 
-            minima fugiat provident, doloremque rerum debitis autem saepe aliquid minus sequi, 
-            reiciendis iste vero quod nesciunt?
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum in fuga quo sunt neque, 
-            minima fugiat provident, doloremque rerum debitis autem saepe aliquid minus sequi, 
-            reiciendis iste vero quod nesciunt?
-        """
-    },
-    {
-        "slug": "going-to-the-beach",
-        "image": "sands.jpg",
-        "author": "Navid",
-        "date": date(2021, 8, 10),
-        "title": "Going to the beach!",
-        "excerpt": "Running on hot sands is an amazing feeling that you can't have with a bitch!",
-        "content":"""
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum in fuga quo sunt neque, 
-            minima fugiat provident, doloremque rerum debitis autem saepe aliquid minus sequi, 
-            reiciendis iste vero quod nesciunt?
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum in fuga quo sunt neque, 
-            minima fugiat provident, doloremque rerum debitis autem saepe aliquid minus sequi, 
-            reiciendis iste vero quod nesciunt?
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum in fuga quo sunt neque, 
-            minima fugiat provident, doloremque rerum debitis autem saepe aliquid minus sequi, 
-            reiciendis iste vero quod nesciunt?
-        """
-    }
-]
-
-def get_key(post):
-    return post['date']
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
 
+
 def index(request):
-    sorted_post = sorted(all_posts, key=get_key)
-    latest_posts = sorted_post[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
+
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
+
 def post_detail(request, slug):
-    selected_post = next(post for post in all_posts if post['slug'] == slug)
+    selected_post = get_object_or_404(Post, slug=slug)
+    # selected_post = Post.objects.get(slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": selected_post
+        "post": selected_post,
+        "post_tags": selected_post.tag.all(),
     })
