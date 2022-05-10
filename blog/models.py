c530b0264dde52bbccb8fae010d1d3d513582684
Navid <navidkaen@gmail.com>
2022-05-10 12:55:06 +0430

    Add Comments model, view

--- blog/models.py
+++ blog/models.py
@@ -36,3 +36,9 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title}, {self.date}, {self.author}, {self.tag}"
+
+class Comment(models.Model):
+    username = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
