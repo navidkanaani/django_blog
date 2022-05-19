cfa491fd8b30219d97b97fa2e9fda796a87121ea
Navid <navidkaen@gmail.com>
2022-05-19 14:20:44 +0430

    Add comment form

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from xml.etree.ElementTree import Comment
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Post
 from .forms import CommentForm
@@ -27,15 +29,38 @@ class AllPostsView(ListView):
     ordering = ["-date"]
 
 
-class PostDetailView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class PostDetailView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tag.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tag.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("specific-post", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tag.all(),
+            "comment_form": comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["post_tags"] = self.object.tag.all()
+    #     context["comment_form"] = CommentForm()
+    #     return context
 
 
 # def index(request):
