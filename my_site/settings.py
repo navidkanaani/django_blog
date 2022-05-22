12f027cad38bcf18f2117b78940e2061ffffef6d
Navid <navidkaen@gmail.com>
2022-05-22 11:21:38 +0430

    Add requirements.txt

--- my_site/settings.py
+++ my_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k-3=8c^6@$j^ux_c4kxd^gpnm5uavg1*rh*la2==tu@^g%%%pg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -120,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
